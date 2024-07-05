92d7ca7c8aa85ae035befab3fc263835e563ae3a
Lijia-Dong <16097601@qq.com>
2024-07-05 17:25:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lijia.cyou": dockerHub,
+  "quay.lijia.cyou": "https://quay.io",
+  "gcr.lijia.cyou": "https://gcr.io",
+  "k8s-gcr.lijia.cyou": "https://k8s.gcr.io",
+  "k8s.lijia.cyou": "https://registry.k8s.io",
+  "ghcr.lijia.cyou": "https://ghcr.io",
+  "cloudsmith.lijia.cyou": "https://docker.cloudsmith.io",
+  "ecr.lijia.cyou": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.lijia.cyou": dockerHub,
 };
 
 function routeByHosts(host) {
